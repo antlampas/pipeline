97b313d96b7e29809a103f7940f3163bc1a7a05e
antlampas <francescoantonetti1@gmail.com>
2020-06-17 19:13:16 +0200

    Updates

--- main.py
+++ main.py
@@ -4,30 +4,9 @@ from oggetto         import oggetto
 
 ipc            = Queue()
 lock_ipc       = Lock()
+file_configurazione = "pipeline.conf"
 configurazione = []
-termina        = 0
-o = oggetto(configurazione,ipc,lock_ipc)
+with open(file_configurazione) as f:
+    configurazione = f.readlines()
 
-o.start()
-while True:
-    comando           = input("Invia comando: ")
-    destinatario      = input("A: ")
-    pacchetto_segnale = comando + ":" + str(time()) + ":" + __name__ + ":" + destinatario
-    with lock_ipc:
-        ipc.put_nowait(pacchetto_segnale)
-        pacchetto_segnale = ""
-    with lock_ipc:
-        if not ipc.empty():
-            pacchetto_segnale    = ipc.get_nowait()
-            segnale_spacchettato = pacchetto_segnale.split(":")
-            print(segnale_spacchettato)
-            if segnale_spacchettato[0] == "terminato":
-                termina = 1
-            else:
-                ipc.put_nowait(pacchetto_segnale)
-                pacchetto_segnale       = ""
-                segnale_spacchettato[:] = []
-    if termina:
-        break
-    sleep(0.001)
-o.join()
+configurazione = [x.strip() for x in configurazione]
